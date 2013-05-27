a2d0bad3fd5af29924af3dae92f0b267c501d173
shekhar <shekhar.chandra@outlook.com>
2013-05-27 23:50:54 +0530

    basic worldbank API call to populate and display a list of all countries

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,22 @@
 /*
  * GET home page.
  */
+var request = require('request');
+var countries = [];
 
-exports.index = function(req, res){
-  res.render('index', { title: 'statMatch' });
+exports.index = function (req, res) {
+    if (countries.length) {
+        console.log(countries.length);
+        res.render('index', { title: 'statMatch', countryList: countries });
+    } else {
+        request({ uri: 'http://api.worldbank.org/countries/?format=json&per_page=250', json: true }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                for (var i = 0; i < body[1].length; i++) {
+                    countries.push(body[1][i]['name']);
+                }
+            }
+            console.log(countries.length);
+            res.render('index', { title: 'statMatch', countryList: countries });
+        });
+    }
 };
